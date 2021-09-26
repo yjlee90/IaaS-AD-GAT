a8685529c553a1f22fc0e86c124a36c49093d797
yjlee90 <olrai011@gmail.com>
2021-09-26 22:39:07 +0900

    preprocess 안해도 될듯

--- my_preprocess.py
+++ my_preprocess.py
@@ -16,18 +16,15 @@ for file in file_list_py :
     vm1 = pd.read_csv(path + file, usecols=['mean_cpu_usage'])
     if vm1.shape[0] < 12780:
         continue
-
     vm2 = vm1.copy()
     vm2['mean_cpu_usage'] = 0 
-
-    print(len(vm1[vm1['mean_cpu_usage']>0.1]))
     vm1 = vm1.append(vm1).append(vm1).reset_index()
-    anomaly = [0.9 * np.sin(x * np.pi/24) for x in range(24)]
+    # anomaly = [0.9 * np.sin(x * np.pi/240) for x in range(240)]
 
     # vm1['is_anomaly'] = 0
 
-    for i in range(24):
-        vm1['mean_cpu_usage'][132+i::12*24] += anomaly[i]
+    # for i in range(240):
+    #     vm1['mean_cpu_usage'][1320+i::12*240] += anomaly[i]
         # vm1['is_anomaly'][132+i::12*24] = 1
 
     vm1[vm1['mean_cpu_usage']>1] = 1
@@ -46,5 +43,26 @@ for file in file_list_py :
 
 
 # %%
+# Data check
+import matplotlib.pyplot as plt
+import pandas as pd
+from random import randint
+
+number = randint(0,65)
+data_path = './data/processed/'
+file_list = [host for host in os.listdir(data_path) if host.endswith('pkl')]
+print(file_list[number])
+host_sample_data = pd.read_pickle(f'{data_path}/{file_list[number]}')
+
+
+# gca stands for 'get current axis'
+# ax = plt.gca()
+host_sample_data.iloc[:,:].plot(kind='line',y='mean_cpu_usage')
+# host_sample_data.plot(kind='line',x='name',y='mean_cpu_usag', color='red', ax=ax)
+
+plt.show()
+
+    
+
 
 # %%
